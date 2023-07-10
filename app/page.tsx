12b5ca7389fef0134470a9880083896ba16991cd
Rounak-28 <rounakkumbhakar2004@gmail.com>
2023-07-10 20:51:10 +0530

    supabase

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,18 @@ import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import Post from '@/components/Post'
 import Image from 'next/image'
-// import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js'
 
 
 export default async function Page() {
 
-  // const supabase_url: any = process.env.NEXT_PUBLIC_SUPABASE_URL
-  // const supabase_api: any = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-  // const supabase = createClient(
-  //   supabase_url, 
-  //   supabase_api,
-  //   { auth: { persistSession: false } }
-  //   )
+  const supabase_url: any = process.env.NEXT_PUBLIC_SUPABASE_URL
+  const supabase_api: any = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  const supabase = createClient(
+    supabase_url, 
+    supabase_api,
+    { auth: { persistSession: false } }
+    )
 
   // const { data: posts } = await supabase.from("posts").select();
   // console.log(posts)
