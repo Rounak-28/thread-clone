ecff21abe1f80e673930ef6fc19ba445278b13d7
Rounak-28 <rounakkumbhakar2004@gmail.com>
2023-07-17 20:49:01 +0530

    show time in every post

--- app/post/[...pid]/page.tsx
+++ app/post/[...pid]/page.tsx
@@ -5,6 +5,7 @@ import { BsChat, BsHeart } from "react-icons/bs";
 import { Suspense } from "react";
 import CommentLoading from "@/components/CommentLoading";
 import Navbar from "@/components/Navbar";
+import { formatDistanceToNow } from "date-fns";
 
 export const revalidate = 0;
 
@@ -19,13 +20,20 @@ export default async function Page({ params }: { params: { pid: string } }) {
 
   // console.log(post)
 
+  const relativeTime = formatDistanceToNow(new Date(post?.created_at), {
+    addSuffix: true,
+  }).replace("about", "");
+
   return (
     <div className="min-h-screen w-screen">
       <Navbar text="Thread"/>
       <div className="border-b-[1px] border-[#3b3b3b] px-2 py-2">
         <div className="profile flex items-center space-x-3 py-2">
           <img src={post?.poster_dp} className="img h-8 w-8 rounded-full" />
-          <span className="text-sm font-semibold">{post?.user_name}</span>
+          <div className="w-screen flex justify-between">
+              <p className="text-sm font-semibold">{post?.user_name}</p>
+              <p className="text-sm">{relativeTime}</p>
+            </div>
         </div>
         <div className="pt-2 text-sm">{post?.content_text}</div>
         <div className="likeanscomms text-xl flex items-center justify-center h-10 space-x-6">
