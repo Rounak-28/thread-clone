b9d6c5087ef8fddfab8fb482d436e560d044ea17
Rounak-28 <rounakkumbhakar2004@gmail.com>
2023-07-23 20:04:19 +0530

    something

--- components/ReplyBox.tsx
+++ components/ReplyBox.tsx
@@ -1,12 +1,12 @@
 const ReplyBox = () => {
   return (
-    <div className="fixed bottom-0 flex justify-center items-center w-screen h-20 bg-dark">
+    <div className="fixed bottom-0 flex justify-center items-center w-screen h-20 bg-dark space-x-2">
       <input
         type="text"
         placeholder="Reply to ..."
-        className="w-[95%] px-4 text-sm placeholder-gray-300 rounded-full h-12 bg-[#2c2c2d] outline-none focus:placeholder-gray-100"
+        className="w-[80%] px-4 text-sm placeholder-gray-300 rounded-full h-12 bg-[#2c2c2d] outline-none focus:placeholder-gray-100"
       />
-      {/* <p>post</p> */}
+      <div className="bg-blue-500 rounded hover:bg-blue-600 w-16 h-10 flex justify-center items-center">post</div>
     </div>
   );
 };
