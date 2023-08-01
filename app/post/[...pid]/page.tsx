445b162a32c2262f3b94e2a64d11baaf89aa87d5
Rounak-28 <rounakkumbhakar2004@gmail.com>
2023-08-01 19:26:13 +0530

    some experiment

--- app/post/[...pid]/page.tsx
+++ app/post/[...pid]/page.tsx
@@ -28,7 +28,7 @@ export default async function Page({ params, searchParams }: any) {
   }).replace("about", "");
 
   return (
-    <div className="h-screen w-screen animate-slide-in">
+    <div className="h-screen w-screen ">
       <Navbar text="Thread" />
       <div className="px-3 py-2">
         <div className="profile flex items-center space-x-3 py-2">
